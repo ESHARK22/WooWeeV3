e8e3c197c9b47dd2f67a1c53a6ef9a231eaf69bd
unity@eshark.co.uk <>
2026-08-29 23:53:13 +0100

    Add leaderboard backend

--- Assets/Player/CounterManager.cs
+++ Assets/Player/CounterManager.cs
@@ -1,10 +1,15 @@
 using TMPro;
+using Unity.Services.Core;
 using UnityEngine;
+using System;
+using Unity.Services.Authentication;
+using Unity.Services.Leaderboards;
 
 public class CounterManager : MonoBehaviour
 {
     public TextMeshProUGUI counterText;
     public GameObject counterParent;
+    private double previous_score = 0;
 
     void Start()
     {
@@ -17,12 +22,61 @@ public class CounterManager : MonoBehaviour
             counterParent.SetActive(false);
         }
     }
+    
+    async void Awake()
+    {
+        Debug.Log("Setup!");
+        Debug.Log("Initializing Services");
+        await UnityServices.InitializeAsync();
+        
+        
+        AuthenticationService.Instance.SignedIn += () => {
+            // Shows how to get a playerID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
 
-    void Update()
+            // Shows how to get an access token
+            Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
+
+        };
+
+        AuthenticationService.Instance.SignInFailed += (err) => {
+            Debug.LogError(err);
+        };
+
+        AuthenticationService.Instance.SignedOut += () => {
+            Debug.Log("Player signed out.");
+        };
+
+        AuthenticationService.Instance.Expired += () =>
+        {
+            Debug.Log("Player session could not be refreshed and expired.");
+        };
+        
+        
+        try
+        {
+            if (AuthenticationService.Instance.IsSignedIn) return;
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    async void Update()
     {
         if (MainMenu.endlessMode)
         {
             counterText.text = GameResult.currentLoop.ToString();
+        }
+    
+        if (previous_score != GameResult.currentLoop)
+        {
+            previous_score = GameResult.currentLoop;
+            var scoreResponse =
+                await LeaderboardsService.Instance.AddPlayerScoreAsync("woowee_leaderboard", GameResult.currentLoop);
+
         }
     }
 }
